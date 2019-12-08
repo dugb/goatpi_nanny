86aae633ccfbdcc5f7d7efb29cfda7b427d01041
dugb <dugbdev@gmail.com>
2019-12-08 14:28:37 -0500

    fileHandler.js

--- fileHandler.js
+++ fileHandler.js
@@ -2,14 +2,12 @@
  * Handles file operations ie getting most recent file in a folder.
  */
 
-const fs = require('fs')
+const fs = require('fs');
 const config = require('./config');
 
-const SOURCE_DIR /** @type {string} */ = config.testImagesLocation;
-
 module.exports = class FileHandler {
-  constructor(src = undefined) {
-    this.sourceDir = src || SOURCE_DIR;
+  constructor() {
+
   }
 
   /**
